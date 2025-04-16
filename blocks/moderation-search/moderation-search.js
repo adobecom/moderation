e2a340181a9db7baa54fdfb34abd76d0fa672118
William Ste-Marie <williambsm@hotmail.fr>
2025-04-16 17:37:47 -0400

    block(moderation-search): add search icon

--- blocks/moderation-search/moderation-search.js
+++ blocks/moderation-search/moderation-search.js
@@ -2,6 +2,7 @@ import { LIBS } from '../../scripts/scripts.js';
 
 const { createTag } = await import(`${LIBS}/utils/utils.js`);
 const { buildArticleCard } = await import(`${LIBS}/blocks/article-feed/article-helpers.js`);
+const SEARCH_ICON = '<svg class="moderation-search-input-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" focusable="false"><path d="M14 2A8 8 0 0 0 7.4 14.5L2.4 19.4a1.5 1.5 0 0 0 2.1 2.1L9.5 16.6A8 8 0 1 0 14 2Zm0 14.1A6.1 6.1 0 1 1 20.1 10 6.1 6.1 0 0 1 14 16.1Z"></path></svg>';
 
 async function fetchArticles() {
   let articles;
@@ -20,8 +21,13 @@ function resetModerationSearch(block) {
 }
 
 function buildSearch(block, placeholderText) {
+  const searchInputContainer = createTag('div', { class: 'moderation-search-input-container' });
+  block.append(searchInputContainer);
   const searchInput = createTag('input', { class: 'moderation-search-input', placeholder: placeholderText });
-  block.append(searchInput);
+  searchInputContainer.append(searchInput);
+  const searchInputIcon = createTag('svg');
+  searchInputContainer.append(searchInputIcon);
+  searchInputIcon.outerHTML = SEARCH_ICON;
   const resultsCountElement = createTag('span', { class: 'moderation-search-results-count' });
   block.append(resultsCountElement);
   const resultsContainer = createTag('div', { class: 'moderation-search-results-container' });
